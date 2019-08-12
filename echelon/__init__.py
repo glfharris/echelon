94ddf5a558a21fec573eb8c49f5fe1d739e44eed
George L. F. Harris <glfharris@gmail.com>
2019-08-12 10:02:02 +0100

    Unicode and Depth Expansions

--- echelon/__init__.py
+++ echelon/__init__.py
@@ -3,7 +3,8 @@ from aqt import mw
 config = mw.addonManager.getConfig(__name__)
 
 # Separator used between hierarchies
-SEPARATOR = config['separator']
+SEPARATOR = config["separator"]
+DEPTH = config["default_depth"]
 
 
 from . import echelon
--- echelon/echelon.py
+++ echelon/echelon.py
@@ -4,6 +4,7 @@ from aqt.qt import QIcon, QAction, QMenu, QCursor, QInputDialog, QLineEdit, QMes
 from anki.hooks import wrap
 
 from . import SEPARATOR
+from . import DEPTH
 from .rename import rename, verify
 
 
@@ -13,23 +14,28 @@ def _userTagTree(self, root, _old):
 
     for t in tags:
         if isParent(t):
-            def fil_func(partial_tag=t): return self.setFilter(
-                "(tag:" + partial_tag + ' or tag:' + partial_tag + SEPARATOR + '*)')
-        else:
-            def fil_func(partial_tag=t): return self.setFilter(
-                "tag", partial_tag)
 
-        if genParent(t) == '':
+            def fil_func(partial_tag=t):
+                return self.setFilter(
+                    "(tag:" + partial_tag + " or tag:" + partial_tag + SEPARATOR + "*)"
+                )
+
+        else:
+
+            def fil_func(partial_tag=t):
+                return self.setFilter("tag", partial_tag)
+
+        if genParent(t) == "":
             parent = root
         else:
             parent = tags_tree[genParent(t)]
 
-        item = self.CallbackItem(
-            parent, t,
-            fil_func)
+        item = self.CallbackItem(parent, t, fil_func)
         item.setIcon(0, QIcon(":/icons/tag.svg"))
 
         tags_tree[t] = item
+
+    self.sidebarTree.expandToDepth(DEPTH)
 
 
 def isParent(tag):
@@ -43,7 +49,7 @@ def isParent(tag):
 def genParent(tag):
     parts = tag.split(SEPARATOR)
     if len(parts) < 2:
-        return ''
+        return ""
     else:
         return SEPARATOR.join(parts[:-1])
 
@@ -53,7 +59,7 @@ def getHier(name):
     parts = name.split(SEPARATOR)
 
     for x in range(len(parts)):
-        res.append(SEPARATOR.join(parts[:(x+1)]))
+        res.append(SEPARATOR.join(parts[: (x + 1)]))
     return res
 
 
@@ -68,10 +74,10 @@ def customMenuEvent(self, event):
     try:
         event.button = lambda: False
         self.menu = QMenu(self)
-        a_rename = QAction('Rename Tag', self)
+        a_rename = QAction("Rename Tag", self)
         a_rename.triggered.connect(lambda x: renameAction(self, event))
         self.menu.addAction(a_rename)
-    # add other required actions
+        # add other required actions
         self.menu.popup(QCursor.pos())
     except:
         pass
@@ -79,18 +85,16 @@ def customMenuEvent(self, event):
 
 def renameAction(self, event):
     row = self.sidebarTree.currentItem().text(0)
-    print(row)
-    q, status = QInputDialog.getText(
-        self, 'Rename Tag', 'Rename Tag:"%s" as:' % row)
+    q, status = QInputDialog.getText(self, "Rename Tag", 'Rename Tag:"%s" as:' % row)
     if status and verify(q):
         rename(row, q)
     elif status and not verify(q):
-        QMessageBox.about(self, 'Title', 'Invalid Tag Name')
+        QMessageBox.about(self, "Title", "Invalid Tag Name")
 
 
 def modInit(self, mw):
     self.sidebarTree.contextMenuEvent = lambda x: customMenuEvent(self, x)
 
 
-Browser._userTagTree = wrap(Browser._userTagTree, _userTagTree, 'around')
-Browser.__init__ = wrap(Browser.__init__, modInit, 'after')
+Browser._userTagTree = wrap(Browser._userTagTree, _userTagTree, "around")
+Browser.__init__ = wrap(Browser.__init__, modInit, "after")
